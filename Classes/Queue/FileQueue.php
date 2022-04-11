2a133be8c6869f68a58927b7d4cc7dd50a64aa66
Stefan Lamm <s.lamm@andersundsehr.com>
2022-04-11 14:04:53 +0200

    [FEATURE] introduces queue limit

--- Classes/Queue/FileQueue.php
+++ Classes/Queue/FileQueue.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Pluswerk\Sentry\Queue;
 
 use Exception;
+use FilesystemIterator;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class FileQueue implements QueueInterface
 {
     private string $directory;
-    public function __construct()
+    private int $limit;
+    private bool $compress = true;
+    public function __construct(int $limit, bool $compress = true)
     {
         $this->directory =  Environment::getVarPath() . '/tx_plussentry_queue/';
+        $this->limit = $limit;
         if (!file_exists($this->directory)) {
             try {
                 GeneralUtility::mkdir_deep($this->directory);
@@ -38,11 +42,25 @@ class FileQueue implements QueueInterface
         }
         if ($file) {
             $absFile = $this->directory . $file;
-            $content = file_get_contents($absFile);
+            // $content = file_get_contents($absFile);
+            $fp = fopen($absFile, 'rb');
+            $mime = mime_content_type($absFile);
+            switch ($mime) {
+                case 'application/json':
+                    break;
+                case 'application/octet-stream':
+                    @stream_filter_append($fp, 'zlib.inflate', STREAM_FILTER_READ);
+                    break;
+                default:
+            }
+            $content = stream_get_contents($fp);
+            fclose($fp);
+
             unlink($absFile);
             if (!$content) {
                 return null;
             }
+
             $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
             if (!$data) {
                 return null;
@@ -62,7 +80,23 @@ class FileQueue implements QueueInterface
         if (!$data) {
             return;
         }
+
+        if ($this->limit) {
+            $fileCount = iterator_count(new FilesystemIterator($this->directory, FilesystemIterator::SKIP_DOTS));
+            if ($fileCount > $this->limit) {
+                return;
+            }
+        }
+
         $fileName = $this->directory . microtime(true) . md5($data) . '.entry';
-        @file_put_contents($fileName, $data);
+        $fp = fopen($fileName, 'wb');
+        if (!$fp) {
+            return;
+        }
+        if ($this->compress) {
+            @stream_filter_append($fp, 'zlib.deflate', STREAM_FILTER_WRITE);
+        }
+        @fwrite($fp, $data);
+        @fclose($fp);
     }
 }
